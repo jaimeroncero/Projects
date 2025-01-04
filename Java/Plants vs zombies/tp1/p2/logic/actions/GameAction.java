0bc348fd4148ccd3ed9512ff42a15c88319a5a6d
jaimixu1 <131661445+jaimixu1@users.noreply.github.com>
2025-01-04 16:27:00 +0100

    update

--- Java/Plants vs zombies/tp1/p2/logic/actions/GameAction.java
+++
@@ -1,8 +0,0 @@
-package tp1.p2.logic.actions;
-
-import tp1.p2.control.exceptions.GameException;
-import tp1.p2.logic.GameWorld;
-
-public interface GameAction {
-	void execute(GameWorld game) throws GameException;
-}
